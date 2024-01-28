aaef48fca78c1979119a89fa3b8417c00d1ec648
deemaagog <deemaagog@gmail.com>
2024-01-28 16:16:56 +0300

    docs: add musicxml selector example

--- docs/src/components/Demo/index.js
+++ docs/src/components/Demo/index.js
@@ -1,26 +1,51 @@
-import React, { useEffect, useRef } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import ScoreStorm from "@score-storm/core"
 import CanvasRenderer from "@score-storm/canvas-renderer"
-import textXml from "./basic-musicxml"
+import basicMusicXml from "./musicxml/basic"
+import basicAccidentalsMusicXml from "./musicxml/basic-accidentals"
+import partsBasicMusicXml from "./musicxml/parts-basic"
+
+const musicalXMLOptions = {
+    "basic": basicMusicXml,
+    "accidentals": basicAccidentalsMusicXml,
+    "parts": partsBasicMusicXml
+}
+
 
 function Demo() {
-    const ref = useRef(null)
+    const rootElementRef = useRef(null)
+    const [selectedMusicXml, setSelectedMusicXml] = useState("basic")
 
+    const scoreStorm = useRef(new ScoreStorm())
 
     useEffect(() => {
-        if (!ref.current) {
+        if (!rootElementRef.current) {
             return
         }
 
-        const scoreRenderer = new ScoreStorm()
-        scoreRenderer.fromMusicXML(textXml)
-        scoreRenderer.setRenderer(new CanvasRenderer(ref.current))
-        scoreRenderer.render()
+        scoreStorm.current.setRenderer(new CanvasRenderer(rootElementRef.current))
 
-    }, [ref])
+    }, [rootElementRef])
+
+    useEffect(() => {
+        if (!rootElementRef.current) {
+            return
+        }
+
+        scoreStorm.current.fromMusicXML(musicalXMLOptions[selectedMusicXml])
+        scoreStorm.current.render()
+
+    }, [rootElementRef, selectedMusicXml])
 
     return (
-        <div style={{ width: "100%" }} ref={ref} />
+        <div style={{ width: "100%" }}>
+            <button style={{ marginRight: "16px" }}>+ Add bar</button>
+            Music xml example:
+            <select name="select" value={selectedMusicXml} onChange={e => setSelectedMusicXml(e.target.value)}>
+                {Object.keys(musicalXMLOptions).map((key) => <option value={key}>{key}</option>)}
+            </select>
+            <div style={{ width: "100%", marginTop: "16px", border: "1px solid #efefef" }} ref={rootElementRef} />
+        </div >
     )
 }
 
