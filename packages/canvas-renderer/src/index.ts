bb13c832d1e30326e0a4183946cb8f17cb145c6f
deemaagog <deemaagog@gmail.com>
2024-02-06 20:26:54 +0100

    refactor: refactor renderer strategy

--- packages/canvas-renderer/src/index.ts
+++ packages/canvas-renderer/src/index.ts
@@ -1,118 +1,49 @@
-import { Renderer, RenderParams, GraphicalScore } from "@score-storm/core"
-
-function getText(unicodeSymbol: string) {
-  const codeString = parseInt(unicodeSymbol.replace("U+", ""), 16)
-  return String.fromCharCode(codeString)
-}
-
-function getTimeSignatureSymbol(n: number) {
-  const map = {
-    0: "U+E080",
-    1: "U+E081",
-    2: "U+E082",
-    3: "U+E083",
-    4: "U+E084",
-    5: "U+E085",
-    6: "U+E086",
-    7: "U+E087",
-    8: "U+E088",
-    9: "U+E089",
-  }
-
-  // @ts-expect-error temp
-  return map[n] || map[0]
-}
+import { Renderer } from "@score-storm/core"
 
 class CanvasRenderer implements Renderer {
   containerElement: HTMLDivElement
   containerWidth: number
   canvasElement: HTMLCanvasElement
+  context: CanvasRenderingContext2D
 
   constructor(containerElement: HTMLDivElement) {
-    console.log("containerElement", containerElement.clientWidth, containerElement.offsetWidth)
     this.containerElement = containerElement
     this.containerWidth = this.containerElement.clientWidth
     this.canvasElement = document.createElement("canvas")
+    this.context = this.canvasElement.getContext("2d")!
     this.containerElement.appendChild(this.canvasElement)
-    
+
     this.setupResizeObserver()
   }
-  
+
   setupResizeObserver() {
     new ResizeObserver((entries) => {
       this.containerWidth = entries[0].contentRect.width
-      console.log("this.containerWidth", this.containerWidth)
     }).observe(this.containerElement) // todo: unobserve?
   }
-  
-  render(graphicalScore: GraphicalScore, params: RenderParams) {
+
+  prepare(height: number, fontSize: number) {
     this.canvasElement.width = this.containerWidth
-    this.canvasElement.height = graphicalScore.height // temp for demo
-    // https://opentype.js.org/glyph-inspector.html
+    this.canvasElement.height = height
+    this.context.font = `${fontSize}px Bravura`
+    this.context.textBaseline = "alphabetic" // middle
+    this.context.textAlign = "start"
+  }
 
-    // notes on glyph positioning
-    // UPM = 1/4 stave space, i.e 48px font size = 12px between stave lines
-    // use  baseline = middle everywhere, default position is middle of 5 lines stave,
-    // unless other specified (g clef + 1 stave space, barline default vertical position is bottom of 5 lines stave)
-    // staveline thickness: 64px font size equals to 2px thickness. Staveline Y position should be adjusted: y - thickness/2
+  clear() {
+    // noop for now
+  }
 
-    const context = this.canvasElement.getContext("2d")!
-    // context.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height) // temp
-    context.font = `${params.fontSize}px Bravura`
+  setColor(color: string) {
+    this.context.fillStyle = color
+  }
 
-    
+  drawRect(x: number, y: number, width: number, height: number) {
+    this.context.fillRect(x, y, width, height)
+  }
 
-    let x = 0
-    let y = 0
-
-    for (let ri = 0; ri < graphicalScore.rows.length; ri++) {
-      const latestRow = ri === graphicalScore.rows.length - 1
-      const row = graphicalScore.rows[ri]
-
-      for (let mi = 0; mi < row.measures.length; mi++) {
-        const latestMeasureInRow = mi === row.measures.length - 1
-        const graphicalMeasure = row.measures[mi]
-
-        // draw stafflines
-        context.fillStyle = "#666666"
-        const half = Math.floor(params.numberOfStaffLines / 2)
-        for (let index = -half; index <= half; index++) {
-          context.fillRect(
-            x,
-            y + params.midStave + params.unit * index - params.staffLineThickness / 2,
-            graphicalMeasure.width,
-            params.staffLineThickness,
-          )
-        }
-
-        context.fillStyle = params.mainColor
-        // draw barline
-        context.fillRect(x, y, params.barLineThickness, params.barlineHeight)
-
-        context.textBaseline = "alphabetic" // middle
-        context.textAlign = "start"
-        context.fillText(getText("U+E4E3"), x + graphicalMeasure.width / 2, y + params.midStave) //whole rest
-
-        if (graphicalMeasure.globalMeasure.time) {
-          const { count, unit } = graphicalMeasure.globalMeasure.time
-          context.fillText(getText(getTimeSignatureSymbol(count)), x + params.unit, y + params.midStave - params.unit) //
-          context.fillText(getText(getTimeSignatureSymbol(unit)), x + params.unit, y + params.midStave + params.unit) //
-        }
-
-        x += graphicalMeasure.width
-        // draw end barline
-        if (latestMeasureInRow) {
-          if (latestRow) {
-            context.fillRect(x - params.unit, y, params.barLineThickness, params.barlineHeight)
-            context.fillRect(x - params.barLineThickness * 3.8, y, params.barLineThickness * 3.8, params.barlineHeight)
-          } else {
-            context.fillRect(x - params.barLineThickness, y, params.barLineThickness, params.barlineHeight)
-          }
-        }
-      }
-      x = 0
-      y += row.height
-    }
+  drawGlyph(glyph: string, x: number, y: number) {
+    this.context.fillText(glyph, x, y)
   }
 
   test() {
