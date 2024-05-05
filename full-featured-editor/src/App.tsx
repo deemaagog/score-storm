2d4b6a666562c5a48475463287b8b7b1ca9c12ee
deemaagog <deemaagog@gmail.com>
2024-05-05 09:44:08 +0200

    wip

--- full-featured-editor/src/App.tsx
+++ full-featured-editor/src/App.tsx
@@ -1,16 +1,21 @@
 import "@mantine/core/styles.css"
 import "./App.css"
-import { Button, AppShell, Stack, useMantineTheme } from "@mantine/core"
-import { useEffect, useRef, useState } from "react"
+import { AppShell, useMantineTheme } from "@mantine/core"
+import { createContext, useEffect, useRef, useState } from "react"
 import ScoreStorm, { EventType, InteractionEvent, Score, GraphicalClef } from "@score-storm/core"
 // import Renderer from "@score-storm/svg-renderer"
 import Renderer from "@score-storm/canvas-renderer"
+import styles from "./App.module.css"
+import Aside from "./Aside"
+
+type ScoreStormContextValue = { scoreStorm: ScoreStorm }
+export const ScoreStormContext = createContext<ScoreStormContextValue>({} as ScoreStormContextValue)
 
 export default function App() {
   const theme = useMantineTheme()
   const rootElementRef = useRef(null)
   const scoreStorm = useRef<ScoreStorm>()
-  const [removeMeasureDisabled, setRemoveMeasureDisabled] = useState(true)
+  const [ssInitialized, setSSInitialized] = useState(false)
 
   const handleClick = (event: InteractionEvent) => {
     if (event.object instanceof GraphicalClef) {
@@ -19,12 +24,14 @@ export default function App() {
     }
   }
 
+  console.log("scoreStorm", scoreStorm.current)
+
   useEffect(() => {
     if (!rootElementRef.current) {
       return
     }
     scoreStorm.current! = new ScoreStorm({
-      scale: 100,
+      scale: 80,
       editor: { enable: true, styles: { hoverColor: theme.colors.blue[6] } },
     })
     scoreStorm.current!.setEventListener(EventType.CLICK, handleClick)
@@ -34,44 +41,28 @@ export default function App() {
     scoreStorm.current!.setScore(score)
     scoreStorm.current!.render()
 
+    // setSSInitialized(true)
+
     return () => {
       // destroy on unmount
       scoreStorm.current!.destroy()
     }
   }, [rootElementRef, theme])
 
-  const updateRemoveMeasureDisabled = () => {
-    const score = scoreStorm.current!.getScore()
-    const numberOfMeasures = score.globalMeasures.length
-    setRemoveMeasureDisabled(numberOfMeasures === 1)
-  }
-
-  const handleAddMeasureClick = () => {
-    scoreStorm.current!.getScore().addMeasure()
-    scoreStorm.current!.render()
-    updateRemoveMeasureDisabled()
-  }
-
-  const handleRemoveMeasureClick = () => {
-    const score = scoreStorm.current!.getScore()
-    const numberOfMeasures = score.globalMeasures.length
-    score.removeMeasure(numberOfMeasures - 1)
-    scoreStorm.current!.render()
-    updateRemoveMeasureDisabled()
-  }
-
   return (
-    <AppShell aside={{ width: 300, breakpoint: "md", collapsed: { desktop: false, mobile: true } }} padding="xl">
+    <AppShell
+      aside={{ width: { base: 210, sm: 210, md: 300 }, breakpoint: "xs", collapsed: { desktop: false, mobile: false } }}
+      padding="xl"
+    >
       <AppShell.Main>
         <div id="ss-container" ref={rootElementRef} />
       </AppShell.Main>
-      <AppShell.Aside p="xl">
-        <Stack bg="var(--mantine-color-body)" align="stretch" justify="center" gap="md">
-          <Button onClick={handleAddMeasureClick}>+ Add measure</Button>
-          <Button disabled={removeMeasureDisabled} onClick={handleRemoveMeasureClick}>
-            - Remove measure
-          </Button>
-        </Stack>
+      <AppShell.Aside className={styles.ssEditorAside} p="xl">
+        {rootElementRef.current && (
+          <ScoreStormContext.Provider value={{ scoreStorm: scoreStorm.current! }}>
+            <Aside />
+          </ScoreStormContext.Provider>
+        )}
       </AppShell.Aside>
     </AppShell>
   )
---
+++ full-featured-editor/src/Aside.tsx
@@ -0,0 +1,39 @@
+import { Button, Stack } from "@mantine/core"
+import { useContext, useState } from "react"
+import { ScoreStormContext } from "./App"
+
+export default function Aside() {
+  const { scoreStorm } = useContext(ScoreStormContext)
+
+  console.log("scoreStorm", scoreStorm)
+  const [removeMeasureDisabled, setRemoveMeasureDisabled] = useState(true)
+
+  const updateRemoveMeasureDisabled = () => {
+    const score = scoreStorm.getScore()
+    const numberOfMeasures = score.globalMeasures.length
+    setRemoveMeasureDisabled(numberOfMeasures === 1)
+  }
+
+  const handleAddMeasureClick = () => {
+    scoreStorm.getScore().addMeasure()
+    scoreStorm.render()
+    updateRemoveMeasureDisabled()
+  }
+
+  const handleRemoveMeasureClick = () => {
+    const score = scoreStorm.getScore()
+    const numberOfMeasures = score.globalMeasures.length
+    score.removeMeasure(numberOfMeasures - 1)
+    scoreStorm.render()
+    updateRemoveMeasureDisabled()
+  }
+
+  return (
+    <Stack bg="var(--mantine-color-body)" align="stretch" justify="center" gap="md">
+      <Button onClick={handleAddMeasureClick}>+ Add measure</Button>
+      <Button disabled={removeMeasureDisabled} onClick={handleRemoveMeasureClick}>
+        - Remove measure
+      </Button>
+    </Stack>
+  )
+}
