a2f0ba4202b30c799881e8da6ab5e8745a08904d
deemaagog <deemaagog@gmail.com>
2024-06-15 21:18:58 +0200

    feat: render 8th, 16th, 32nd, 64th notes and rests

--- packages/core/src/graphical/glyphs/rest/index.ts
+++ packages/core/src/graphical/glyphs/rest/index.ts
@@ -20,3 +20,31 @@ export const RestQuarter: Glyph = {
   advancedWidth: 1.08,
   bBoxes: { bBoxNE: [1.08, 1.492], bBoxSW: [0.004, -1.5] },
 }
+
+export const RestEighth: Glyph = {
+  symbol: "U+E4E6",
+  smuflName: "rest8th",
+  advancedWidth: 1,
+  bBoxes: { bBoxNE: [0.988, 0.696], bBoxSW: [0, -1.004] },
+}
+
+export const Rest16th: Glyph = {
+  symbol: "U+E4E7",
+  smuflName: "rest16th",
+  advancedWidth: 1.28,
+  bBoxes: { bBoxNE: [1.28, 0.716], bBoxSW: [0, -2] },
+}
+
+export const Rest32nd: Glyph = {
+  symbol: "U+E4E8",
+  smuflName: "rest32nd",
+  advancedWidth: 1.452,
+  bBoxes: { bBoxNE: [1.452, 1.704], bBoxSW: [0, -2] },
+}
+
+export const Rest64th: Glyph = {
+  symbol: "U+E4E9",
+  smuflName: "res64th",
+  advancedWidth: 1.696,
+  bBoxes: { bBoxNE: [1.692, 1.72], bBoxSW: [0, -3.012] },
+}
