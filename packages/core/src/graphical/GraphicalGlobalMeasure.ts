8c5e2d7ce4e04d1219690400fd64df99b2eea892
deemaagog <deemaagog@gmail.com>
2024-08-24 06:54:46 +0200

    WIP

--- packages/core/src/graphical/GraphicalGlobalMeasure.ts
+++ packages/core/src/graphical/GraphicalGlobalMeasure.ts
@@ -1,5 +1,10 @@
+import Fraction from 'fraction.js';
 import { GlobalMeasure } from "../model/GlobalMeasure"
 import { GraphicalMeasure } from "./GraphicalMeasure"
+
+export class FractionablePlayEvent {
+  fraction!: Fraction
+}
 
 export class GraphicalGlobalMeasure {
   minContentWidth!: number // notes/rests only, measure attributes are not taken into account
