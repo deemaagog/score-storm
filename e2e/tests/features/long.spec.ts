4ad9f7688b69d81f9d95791a4dd7ed7c04b7b0d0
deemaagog <deemaagog@gmail.com>
2025-03-12 23:06:42 +0100

    test: add e2e for long music score

---
+++ e2e/tests/features/long.spec.ts
@@ -0,0 +1,29 @@
+import { expect } from "@playwright/test"
+import fs from "fs"
+import path from "path"
+import { test } from "../parametrized-test"
+
+// todo: move to global setup
+test.beforeEach(async ({ page, renderer }) => {
+  page.setViewportSize({ width: 1600, height: 800 })
+  await page.goto("/")
+  await page.evaluate((renderer) => {
+    window.scoreStorm.setRenderer(renderer === "svg" ? window.svgRenderer : window.canvasRenderer)
+  }, renderer)
+})
+
+// todo: move to global setup
+test.afterEach(async ({ page }) => {
+  await page.evaluate(() => {
+    window.scoreStorm.render()
+  })
+
+  await expect(page.locator("#ss-container")).toHaveScreenshot()
+})
+
+test("correctly renders long score", async ({ page }) => {
+  const inputXmlString = fs.readFileSync(path.join(__dirname, "long.musicxml"), "utf8")
+  await page.evaluate((xml) => {
+    window.scoreStorm.setScore(window.getScoreFormMusicXml(xml))
+  }, inputXmlString)
+})
